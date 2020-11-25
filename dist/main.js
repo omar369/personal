0bf42cac747ecf90654be094f1e9061f91d6e446
omar369 <34948096+omar369@users.noreply.github.com>
2020-11-25 16:09:34 -0600

    Agregando threeJS cubo

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,33 @@
 import * as THREE from 'three';
 import * as BABYLON from 'babylonjs';
 
+//prueba inicial
 alert("Bienvenido Cretino");
-console.log("app corriendo")
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+console.log("app corriendo");
